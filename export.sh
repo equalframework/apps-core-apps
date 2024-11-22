a142eb611e8eb850149efba74c525bd9920d4f23
Cédric Françoys <cedricfrancoys@gmail.com>
2024-11-22 09:33:36 +0100

    added export of web.app & manifest

--- export.sh
+++ export.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-rm -rf ../../../../public/apps && mkdir ../../../../public/apps && cp -a dist/symbiose/* ../../../../public/apps/
+cp -r version ../version && cp -r web.app ../web.app && cp -r manifest.json ../manifest.json
+rm -rf ../../../../../public/apps && mkdir ../../../../../public/apps && cp -a dist/symbiose/* ../../../../../public/apps/
